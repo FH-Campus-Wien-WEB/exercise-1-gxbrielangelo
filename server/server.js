400e3357a564f534f74a0bb4eee4c496e266b5d5
apple <98890898+gxbrielangelo@users.noreply.github.com>
2026-03-24 11:02:04 +0100

    feat: added all movies

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.get("/movies", function (req, res) {
     `{"Title":"Iron Man","Released":"2008-05-02","Runtime":"126","Genres":["Action", "Adventure", "Sci-Fi"],"Directors":["Jon Favreau"],"Writers":["Mark Fergus", "Hawk Ostby", "Art Marcum"],"Actors":["Robert Downey Jr.", "Gwyneth Paltrow", "Terrence Howard"],"Plot":"After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.","Poster":"https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg","Metascore":79,"imdbRating":7.9}`,
   ];
   //res.send("!dlrow olleH");
-  res.send(JSON.parse(movies[0]));
+  res.send(movies);
 });
 
 app.listen(3000);
